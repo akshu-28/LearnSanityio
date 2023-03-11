b8de9ecfed108e35b0ce115bd5bf5dac75b3a41d
akshaya.sivakumar <akshaya.s@marketsimplified.com>
2023-03-11 11:09:01 +0530

    post request imaplementation

--- mobile_client/.dart_tool/dartpad/web_plugin_registrant.dart
+++ mobile_client/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -8,6 +8,7 @@
 
 import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -15,6 +16,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   FilePickerWeb.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
